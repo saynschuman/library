ec7035fe39f775a4b11dbf836146a71274337ced
saynschuman <saynschuman@gmail.com>
2017-08-16 12:50:09 +0300

    layout prepared

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,22 +1,5 @@
 (function($) {
     
-	$('.slider').slick({
-		arrows: false,
-		fade: true,
-	  	cssEase: 'linear',
-	  	autoplay: true,
-	  	autoplaySpeed: 3000
-	})
-
-	$('.sh-m').click(function(){
-		$('.text-inner').addClass('norm-size');
-		$(this).hide();
-	})
-
-	$('.up').click(function(){
-		$('body,html').animate({
-				scrollTop: 0
-			}, 800);
-	})
+	
 
 })(jQuery);
