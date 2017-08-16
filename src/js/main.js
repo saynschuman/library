56963a60dd850d8edeb1ee5e485711cdc70f8fb9
saynschuman <saynschuman@gmail.com>
2017-08-16 20:58:27 +0300

    first section finished

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,20 @@
 		$(this).addClass('active');
 	})
 
+	$('#search-cat input').focus(function(){
+		$('#search-cat').addClass('focus');
+	})
+
+	$('#search-cat input').blur(function(){
+		$('#search-cat').removeClass('focus');
+	})
+
+	 $('input,textarea').focus(function(){
+	   $(this).data('placeholder',$(this).attr('placeholder'))
+	   $(this).attr('placeholder','');
+	 });
+	 $('input,textarea').blur(function(){
+	   $(this).attr('placeholder',$(this).data('placeholder'));
+	 });	
+
 })(jQuery);
